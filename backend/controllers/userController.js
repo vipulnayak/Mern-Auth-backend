1f8ed37ba7f0d38abaa70494747aedb304c0f716
Vipul <vipulnayak1192@gmail.com>
2023-08-17 22:57:13 +0530

    Mongo atlas connected and error handlers added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,48 @@
+import asyncHandler from "express-async-handler";
+
 //@desc AUth user/set token
 //route POST api/users/auth
 //@access Public  
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Auth User' })
-}
+});
+
+
+//@desc Regsiter a new user
+//route POST api/users
+//@access Public  
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Register User' })
+});
+
+
+//@desc logout user
+//route POST api/users/logout
+//@access Public  
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Logout User' })
+});
+
+
+//@desc Get user profile
+//route GET api/users/profile
+//@access Private  
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: ' User Profile' })
+});
+
+
+//@desc Update user profile
+//route PUT api/users/profile
+//@access Private  
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'update User Profile' })
+});
 
 export {
-    authUser
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
 }
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import { notFound, errorHandler } from './middleware/errormiddleware.js';
+import connectDB from '../config/db.js';
 const port = process.env.port || 5000;
 import userRoutes from './routes/userRoutes.js'
 
+
+connectDB();
 const app = express();
 
 app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send('server is ready'))
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server stared on port ${port}`))
