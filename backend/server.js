c87c3dc2add2746f2e7a8e7324434d0fa1821ae2
Vipul <vipulnayak1192@gmail.com>
2023-08-17 15:01:13 +0530

    backend port added

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const port=5000
+
+const app = express();
+
+app.get('/', (req,res)=> res.send('server is ready'))
+
+app.listen(port, ()=> console.log(`server stared on port ${port}`))
