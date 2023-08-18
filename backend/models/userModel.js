f42063c4418a8c8e09245260509099ef7902e01f
Vipul <vipulnayak1192@gmail.com>
2023-08-18 20:57:58 +0530

    register api created with password hashing

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,23 +1,33 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        require:true
+    name: {
+        type: String,
+        require: true
     },
-    email:{
-        type:String,
-        require:true,
-        unique:true
+    email: {
+        type: String,
+        require: true,
+        unique: true
     },
-    password:{
-        type:String,
-        require:true
+    password: {
+        type: String,
+        require: true
     }
-},{
-    timestamps:true
+}, {
+    timestamps: true
 });
 
-const User= mongoose.model('User',userSchema)
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', userSchema)
 
 export default User;
