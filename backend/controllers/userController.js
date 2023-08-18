ba623035d3e9d3ddd2b7b178ad5f6dc244b28589
Vipul <vipulnayak1192@gmail.com>
2023-08-18 22:12:15 +0530

    Login and Logout routes added with cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,27 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
-
+import generateToken from "../utils/generateToken.js";
 
 //@desc AUth user/set token
 //route POST api/users/auth
 //@access Public  
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Auth User' })
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPasswords(password))) {
+        generateToken(res, user._id)
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid email and password');
+    }
 });
 
 //@desc Regsiter a new user
@@ -29,6 +44,7 @@ const registerUser = asyncHandler(async (req, res) => {
     })
 
     if (user) {
+        generateToken(res, user._id)
         res.status(201).json({
             _id: user._id,
             name: user.name,
@@ -45,7 +61,12 @@ const registerUser = asyncHandler(async (req, res) => {
 //route POST api/users/logout
 //@access Public  
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Logout User' })
+    res.cookie('jwt', '',{
+        httpOnly:true,
+        expires: new Date(0)
+    })
+
+    res.status(200).json({message:'User Logged out'});
 });
 
 //@desc Get user profile
