5a7269acfd40c86b437bde7275dbc6516b5269c1
Vipul <vipulnayak1192@gmail.com>
2023-08-19 20:39:28 +0530

    User profile added and some errors solved

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errormiddleware.js';
 import connectDB from './config/db.js';
 const port = process.env.port || 5000;
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+
+app.use(cookieParser());
 
 //basic url for using userRoutes
 app.use('/api/users', userRoutes);
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,7 +5,7 @@ const generateToken = (res, userId) => {
         expiresIn: '30d'
     })
 
-    res.cookie(jwt,token,{
+    res.cookie('jwt',token,{
         httpOnly:true,
         secure: process.env.NODE_ENV !== 'development',
         sameSite:'strict',
